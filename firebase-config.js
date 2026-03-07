f85d119ac22f17501b3813ded80b54f9ad6b826c
MoriahDigital <camarayaya87@gmail.com>
2026-03-07 19:33:13 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAuC2URKTGD6MrnmkdSD3xTKcyS5BPE0e0",
   authDomain: "moriahdigital-141f4.firebaseapp.com",
   projectId: "moriahdigital-141f4",
-  storageBucket: "moriahdigital-141f4.firebasestorage.app",
+  storageBucket: "moriahdigital-141f4.appspot.com",
   messagingSenderId: "743291676320",
   appId: "1:743291676320:web:baf76020c1e36cc5440bc5",
   measurementId: "G-PES2PBLM6S"
