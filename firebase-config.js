68a29dc88a948f307867c517fd55dac1b689bf66
MoriahDigital <camarayaya87@gmail.com>
2026-03-07 17:11:09 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,25 @@
+// Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// Configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAuC2URKTGD6MrnmkdSD3",
+  apiKey: "AIzaSyAuC2URKTGD6MrnmkdSD3xTKcyS5BPE0e0",
   authDomain: "moriahdigital-141f4.firebaseapp.com",
   projectId: "moriahdigital-141f4",
-  storageBucket: "moriahdigital-141f4.appspot.com",
+  storageBucket: "moriahdigital-141f4.firebasestorage.app",
   messagingSenderId: "743291676320",
-  appId: "1:743291676320:web:baf76020"
+  appId: "1:743291676320:web:baf76020c1e36cc5440bc5",
+  measurementId: "G-PES2PBLM6S"
 };
 
+// Initialiser Firebase
 const app = initializeApp(firebaseConfig);
+
+// Activer les services
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+// Exporter pour le site
 export { auth, db };
