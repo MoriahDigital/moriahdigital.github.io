8c6da6451f056f0bfb4275d05251fe8d83744875
MoriahDigital <camarayaya87@gmail.com>
2026-03-08 06:35:07 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAuC2URKTGD6MrnmkdSD3xT",
@@ -7,11 +8,11 @@ const firebaseConfig = {
   projectId: "moriahdigital-141f4",
   storageBucket: "moriahdigital-141f4.appspot.com",
   messagingSenderId: "743291676320",
-  appId: "1:743291676320:web:baf76020c1",
-  measurementId: "G-PES2PBLM6S"
+  appId: "1:743291676320:web:baf76020c1"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { auth };
+export { auth, db };
